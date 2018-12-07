11ad104241d1b111df5ba4c12d9bdd0025f63e72
Masashi Otsuka <masashi1029xxx@gmail.com>
2018-12-07 09:28:15 +0900

    change Source directory

--- src/main/java/com/example/tukapai/UserAddController.java
+++ src/main/java/com/example/tukapai/UserAddController.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.tukapai;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
