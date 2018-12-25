cff453ffc2053033a29928e8e7e84499ee156061
Masashi Otsuka <masashi1029xxx@gmail.com>
2018-12-25 22:40:21 +0900

    add material ui core

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <h1>Hello world!</h1>,
           <a
             className="App-link"
             href="https://reactjs.org"
