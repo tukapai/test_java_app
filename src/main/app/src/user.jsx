912887991472c95f8ab31f579f39c4816ec3f5d1
Masashi Otsuka <masashi1029xxx@gmail.com>
2018-12-27 23:30:59 +0900

    add user.jsx

---
+++ src/main/app/src/user.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var request = require('superagent');
+
+//フォームとリストを一つにしたもの
+var UserBox = React.createClass({
+  getInitialState:function(){
+    return {userData:[]};
+  },
+：
+：
+：
+    );
+  }
+});
+
+module.exports = UserBox;
