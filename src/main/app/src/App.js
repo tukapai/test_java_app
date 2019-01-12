f40224885548f0ca5fc3b9d5527bd473cba362d0
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-01-12 23:37:18 +0900

    Update App.js

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -22,17 +22,17 @@ class App extends Component {
           </a>
         </header>
         <div>
-  <div className="main">
-    <h1>ログイン</h1>
-    <form onSubmit={this.handleSubmit}>
-      <input placeholder="userid"/>
-      <input placeholder="password"/>
-      <div style={{textAlign:"center"}}>
-        <button type="submit">ログイン</button>
-      </div>
-    </form>
-  </div>
-</div>
+         <div className="main">
+           <h1>ログイン</h1>
+           <form onSubmit={this.handleSubmit}>
+             <input placeholder="userid"/>
+             <input placeholder="password"/>
+             <div style={{textAlign:"center"}}>
+               <button type="submit">ログイン</button>
+             </div>
+           </form>
+         </div>
+       </div>
       </div>
     );
   }
