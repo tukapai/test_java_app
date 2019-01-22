7c89931be6dc6eca5cfd3a53c9eb59f9b728e1df
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-01-22 21:25:40 +0900

    add Todo sample

--- src/main/app/src/Form.js
+++ src/main/app/src/Form.js
@@ -8,8 +8,8 @@ export default class Form extends Component {
       <div className="form">
         <form name="todoform" onSubmit={this.props.onSubmit}>
           <input name="title" type="text" placeholder="タイトル ※必須" defaultValue="reactの勉強" /><br/>
-          <textarea name="desc" placeholder="説明を入力" defaultValue="todoアプリを作っています！"></textarea><br/>
-          <button type="submit">todoを作成</button>
+          <textarea name="desc" placeholder="説明を入力" defaultValue="todok工事中！"></textarea><br/>
+          <button type="submit">submit</button>
         </form>
       </div>
     );
